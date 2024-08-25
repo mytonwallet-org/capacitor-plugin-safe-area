0e7d38fc5b6ea810fdc9c0f18d09b1c619d50f40
sina <sina_od@yahoo.com>
2024-08-25 23:45:43 +0330

    Updated android insets

--- android/src/main/java/com/capacitor/safearea/SafeArea.java
+++ android/src/main/java/com/capacitor/safearea/SafeArea.java
@@ -3,12 +3,12 @@ package com.capacitor.safearea;
 import android.util.Log;
 
 import android.os.Build;
-import android.os.View;
 
 import com.getcapacitor.Bridge;
 import com.getcapacitor.JSObject;
 
 import android.view.DisplayCutout;
+import android.view.View;
 import android.view.WindowInsets;
 
 public class SafeArea {
@@ -24,7 +24,7 @@ public class SafeArea {
             return this.result(0, 0, 0, 0);
         }
         DisplayCutout displayCutout = null;
-        WindowInsets windowInsets = this.bridge.getActivity().getWindow().getDecorView().findViewById(android.R.id.content).getRootWindowInsets();
+        WindowInsets windowInsets = this.bridge.getActivity().getWindow().getDecorView().getRootWindowInsets();
         if (windowInsets == null) {
             Log.i(SafeAreaPlugin.class.toString(), "WindowInsets is not available.");
             return this.result(0, 0, 0, 0);
@@ -36,22 +36,12 @@ public class SafeArea {
         int bottom = windowInsets.getStableInsetBottom();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             displayCutout = windowInsets.getDisplayCutout();
-            if(displayCutout != null) {
-                top = displayCutout.getSafeInsetTop();
-                left = displayCutout.getSafeInsetLeft();
-                right = displayCutout.getSafeInsetRight();
-                bottom = displayCutout.getSafeInsetBottom();
-                
-
-                if(!this.getStatusBarVisible()) {
-                    top = Math.max(windowInsets.getStableInsetTop(), top);
-                }
-
-                if(!this.tabBarIsVisible()) {
-                    bottom = Math.max(displayCutout.getSafeInsetBottom(), bottom);
-                }
+            if (displayCutout != null) {
+                top = Math.max(displayCutout.getSafeInsetTop(), top);
+                left = Math.max(displayCutout.getSafeInsetLeft(), left);
+                right = Math.max(displayCutout.getSafeInsetRight(), right);
+                bottom = Math.max(displayCutout.getSafeInsetBottom(), bottom);
             }
-
         }
         return this.result(top, left, right, bottom);
     }
@@ -62,13 +52,13 @@ public class SafeArea {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
             windowInsets = this.bridge.getActivity().getWindow().getDecorView().getRootWindowInsets();
         }
-        return  windowInsets != null && windowInsets.getSystemWindowInsetTop() == 0;
+        return windowInsets != null && windowInsets.getSystemWindowInsetTop() == 0;
     }
 
     // 判断是否是沉浸式导航栏
     private Boolean tabBarIsVisible() {
-        int uiOptions = this.bridge.getWindow().getDecorView().getSystemUiVisibility();
-        return (uiOptions & View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION) == View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        int uiOptions = this.bridge.getActivity().getWindow().getDecorView().getSystemUiVisibility();
+        return (uiOptions & View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION) == View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
     }
 
     public int getStatusBarHeight() {
