979a52df41c6c53bab92f942a418563865a87f0e
sina <sina_od@yahoo.com>
2024-08-26 00:09:32 +0330

    Added dist to fix capacitor app build issue

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,39 @@
+import type { PluginListenerHandle } from '@capacitor/core';
+export interface SafeAreaPlugin {
+    /**
+     *  Get mobile SafeArea info
+     */
+    getSafeAreaInsets(): Promise<SafeAreaInsets>;
+    /**
+     * Get mobile statusbar height
+     */
+    getStatusBarHeight(): Promise<StatusBarInfo>;
+    /**
+     * Set navigation bar immersive on Android , not implemented on IOS
+     */
+    setImmersiveNavigationBar(): Promise<void>;
+    /**
+     * Event listener when safe-area changed
+     * @param event
+     * @param listenerFunc
+     */
+    addListener(event: 'safeAreaChanged', listenerFunc: (data: SafeAreaInsets) => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+    /**
+     * Capacitor plugin method to remove all created listeners
+     * @deprecated use `SafeArea.removeAllListeners()` instead
+     */
+    removeAllListeners(): Promise<void>;
+}
+interface SafeArea {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+}
+export interface SafeAreaInsets {
+    insets: SafeArea;
+}
+export interface StatusBarInfo {
+    statusBarHeight: number;
+}
+export {};
---
+++ dist/plugin.js
@@ -0,0 +1,42 @@
+var capacitorSafeArea = (function (exports, core) {
+    'use strict';
+
+    const SafeArea = core.registerPlugin('SafeArea', {
+        web: () => Promise.resolve().then(function () { return web; }).then(m => new m.SafeAreaWeb()),
+    });
+
+    class SafeAreaWeb extends core.WebPlugin {
+        async getSafeAreaInsets() {
+            return {
+                insets: {
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                }
+            };
+        }
+        async getStatusBarHeight() {
+            // throw this.unimplemented('Method not supported on Web.');
+            return {
+                statusBarHeight: 0
+            };
+        }
+        setImmersiveNavigationBar() {
+            throw this.unimplemented('Method not supported on Web.');
+        }
+    }
+
+    var web = /*#__PURE__*/Object.freeze({
+        __proto__: null,
+        SafeAreaWeb: SafeAreaWeb
+    });
+
+    exports.SafeArea = SafeArea;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+    return exports;
+
+})({}, capacitorExports);
+//# sourceMappingURL=plugin.js.map
